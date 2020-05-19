8a1bd59b62796f9df1086d73d6181d01137df15a
Nazmus Nasir <nazmus.nasir@gmail.com>
2020-05-19 01:30:31 -0400

    removed build/ dir from gitignore since it has the react files

---
+++ build/precache-manifest.14644fd70e86ee4efbda9358d3df83f4.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2710da99ca8534de53451fb081bdf3e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b43808c829b9d9171f4",
+    "url": "/static/css/2.171d7bcd.chunk.css"
+  },
+  {
+    "revision": "e7dfae2cfe3d4939108c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0b43808c829b9d9171f4",
+    "url": "/static/js/2.fb2c935b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.fb2c935b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7dfae2cfe3d4939108c",
+    "url": "/static/js/main.ff7985a9.chunk.js"
+  },
+  {
+    "revision": "a2906e40fc545fc5a235",
+    "url": "/static/js/runtime-main.0c5a3238.js"
+  },
+  {
+    "revision": "c7a33805ffda0d32bd2a9904c8b02750",
+    "url": "/static/media/color.c7a33805.png"
+  },
+  {
+    "revision": "567f57385ea3dde2c9aec797d07850d2",
+    "url": "/static/media/line.567f5738.gif"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "177cc92d2e8027712a8c1724abd272cd",
+    "url": "/static/media/open-sans-v15-latin-300.177cc92d.ttf"
+  },
+  {
+    "revision": "27ef0b062b2e221df16f3bbd97c2dca8",
+    "url": "/static/media/open-sans-v15-latin-300.27ef0b06.svg"
+  },
+  {
+    "revision": "521d17bc9f3526c690e8ada6eee55bec",
+    "url": "/static/media/open-sans-v15-latin-300.521d17bc.woff"
+  },
+  {
+    "revision": "60c866748ff15f5b347fdba64596b1b1",
+    "url": "/static/media/open-sans-v15-latin-300.60c86674.woff2"
+  },
+  {
+    "revision": "76b56857ebbae3a5a689f213feb11af0",
+    "url": "/static/media/open-sans-v15-latin-300.76b56857.eot"
+  },
+  {
+    "revision": "148a6749baa5f658a45183ddb5ee159f",
+    "url": "/static/media/open-sans-v15-latin-700.148a6749.eot"
+  },
+  {
+    "revision": "2e00b2635b51ba336b4b67a5d0bc03c7",
+    "url": "/static/media/open-sans-v15-latin-700.2e00b263.svg"
+  },
+  {
+    "revision": "623e3205570002af47fc2b88f9335d19",
+    "url": "/static/media/open-sans-v15-latin-700.623e3205.woff"
+  },
+  {
+    "revision": "7e08cc656863d52bcb5cd34805ac605b",
+    "url": "/static/media/open-sans-v15-latin-700.7e08cc65.ttf"
+  },
+  {
+    "revision": "d08c09f2f169f4a6edbcf8b8d1636cb4",
+    "url": "/static/media/open-sans-v15-latin-700.d08c09f2.woff2"
+  },
+  {
+    "revision": "7aab4c13671282c90669eb6a10357e41",
+    "url": "/static/media/open-sans-v15-latin-regular.7aab4c13.svg"
+  },
+  {
+    "revision": "9dce7f01715340861bdb57318e2f3fdc",
+    "url": "/static/media/open-sans-v15-latin-regular.9dce7f01.eot"
+  },
+  {
+    "revision": "bf2d0783515b7d75c35bde69e01b3135",
+    "url": "/static/media/open-sans-v15-latin-regular.bf2d0783.woff"
+  },
+  {
+    "revision": "c045b73d86803686f4cd1cc3f9ceba59",
+    "url": "/static/media/open-sans-v15-latin-regular.c045b73d.ttf"
+  },
+  {
+    "revision": "cffb686d7d2f4682df8342bd4d276e09",
+    "url": "/static/media/open-sans-v15-latin-regular.cffb686d.woff2"
+  },
+  {
+    "revision": "082d10b663f484bce57255466066c814",
+    "url": "/static/media/primeicons.082d10b6.eot"
+  },
+  {
+    "revision": "701daee424b3c1872720a820cae7ca30",
+    "url": "/static/media/primeicons.701daee4.ttf"
+  },
+  {
+    "revision": "96b27389bdf7ccfd4d1d3a1199b1bd81",
+    "url": "/static/media/primeicons.96b27389.svg"
+  },
+  {
+    "revision": "a890ff7c52932af01dbbe1082ed5b8a8",
+    "url": "/static/media/primeicons.a890ff7c.woff"
+  }
+]);
